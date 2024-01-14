6fedee9fd036780886f43b24e4558bbcb9af476f
alexVpenev <alexVpenev+github@proton.me>
2024-01-14 19:13:27 +0100

    initialized react app with yarn, and setup working charts

---
+++ src/Data.js
@@ -0,0 +1,20 @@
+export const UserData = [
+  {
+    id: 1,
+    date: 6,
+    kcal: 2300,
+    weight: 115,
+  },
+  {
+    id: 2,
+    date: 7,
+    kcal: 2250,
+    weight: 114,
+  },
+  {
+    id: 3,
+    date: 8,
+    kcal: 2300,
+    weight: 115,
+  },
+];
